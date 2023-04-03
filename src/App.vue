bdefb1ec014971ef90147be48e0465db13483473
xiaoming <1656736939@qq.com>
2023-04-03 22:32:12 +0800

    项目初始化，引入Vant组件库

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,11 @@
     <!-- 路由的出口 -->
     <router-view/>
     <h1>黑马头条</h1>
+    <div>
+      <i class="toutiao toutiao-shipin"></i>
+    </div>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
   </div>
 </template>
 <script>
